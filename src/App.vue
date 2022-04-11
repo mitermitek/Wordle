6bc253aa03c8ea61af96a9a6d0a30fc3e30c7a0e
MitermiteK <40229021+mitermitek@users.noreply.github.com>
2022-04-11 16:34:36 +0200

    Modification message d'alerte perdu
    Modification du message d'alerte perdu pour le mot à trouver. On l'affiche maintenant avec les accents.

--- src/App.vue
+++ src/App.vue
@@ -89,7 +89,7 @@ onMounted(() => {
     <div v-if="lostGame" class="bg-red-100 border-t-4 border-red-500 rounded-b text-red-900 px-4 py-3" role="alert">
       <div class="flex">
         <div>
-          <p class="font-semibold">Perdu ! 🥶 Le mot a trouver était : {{ state.solution }}</p>
+          <p class="font-semibold">Perdu ! 🥶 Le mot a trouver était : {{ word }}</p>
         </div>
       </div>
     </div>
